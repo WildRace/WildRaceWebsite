b6c8ecf32e9b91469c8422610e2995ff2c4d3c83
Daniil Gofman <65250473+danielgof@users.noreply.github.com>
2025-10-26 10:44:50 -0400

    build works!

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game() {
-  return <div>Coming soooooooooooooon.................</div>;
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/Builds.loader.js",
+    dataUrl: "build/Builds.data",
+    frameworkUrl: "build/Builds.framework.js",
+    codeUrl: "build/Builds.wasm",
+  });
+
+  return (
+    <div style={{ width: "960px", height: "600px", margin: "0 auto" }}>
+      <Unity
+        unityProvider={unityProvider}
+        style={{ width: "100%", height: "100%", background: "black" }}
+      />
+    </div>
+  );
 }
 
 export default Game;
